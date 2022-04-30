e0baef21747774095228e3df5be1c658d7ff2061
NikoLee18 <NikoLee1118@outlook.com>
2022-04-30 23:18:33 +0800

    scaling_factor_version

--- build.py
+++ build.py
@@ -3,11 +3,11 @@ from . import toolkit as tk
 import numpy as np
 
 
-def build_Gaussian_HOHMM(K: int, r: int, m: int, dim: int):
+def build_Gaussian_HOHMM(K: int, r: int, m: int, dim: int, init_mean=0, init_sacle=0.1):
 
     nu = max(r, m)
     # init mu and Sigma
-    phi = tk.init_gaussian_emission(K, m, dim)
+    phi = tk.init_gaussian_emission(K, m, dim, init_mean, init_sacle)
     # init transition matrix
     A = tk.init_transit_matrix(K, r, m)
     # init initial distribution
